c361e49ff4cc7740246cb8560b79f8ac412f4fbd
NoussaK <kaabinousseiba11@gmail.com>
2022-10-31 22:45:44 +0100

    first commit

---
+++ Desktop/Gestion_invites/connexion.cpp
@@ -0,0 +1,22 @@
+#include "connexion.h"
+
+connexion::connexion(){}
+
+bool connexion::createconnexion()
+{
+    db=QSqlDatabase::addDatabase("QODBC");
+    bool test=false;
+    db.setDatabaseName("Gestion_invites");
+    db.setUserName("nousseiba");
+    db.setPassword("esprit18");
+
+    if(db.open())
+        test=true;
+    return test;
+}
+
+void connexion::closeconnexion()
+{
+
+    db.close();
+}
---
+++ Desktop/Gestion_invites/connexion.h
@@ -0,0 +1,14 @@
+#ifndef CONNEXION_H
+#define CONNEXION_H
+#include <QSqlDatabase>
+
+class connexion
+{
+    QSqlDatabase db;
+public:
+    connexion();
+    bool createconnexion();
+    void closeconnexion();
+};
+
+#endif // CONNEXION_H
