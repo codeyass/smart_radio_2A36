53885a758b4caddd4e62d7c6793ecce415e5253a
Azizmakhlouf1920 <101336657+Azizmakhlouf1920@users.noreply.github.com>
2022-11-15 09:50:46 +0100

    Create mainwindow.h

---
+++ mainwindow.h
@@ -0,0 +1,36 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+#include "employe.h"
+#include <QMainWindow>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void on_pb_ajouter_clicked();
+
+    void on_pb_supprimer_clicked();
+
+    void on_pb_modifier_clicked();
+
+    void on_PDF_pb_clicked();
+
+    void on_pushButton_clicked();
+
+
+
+private:
+    Ui::MainWindow *ui;
+    employe E,Etmp;
+};
+
+#endif // MAINWINDOW_H
